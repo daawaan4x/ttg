d5ca58448df9568fe0e7a37a1eb34ff403c96807
daawaan4U <theoneeclarin@outlook.com>
2024-10-19 13:35:01 +0800

    hotfix: add interactive mode

--- ttg/command.py
+++ ttg/command.py
@@ -6,13 +6,23 @@ import click
 from ttg.console import rich_console
 from ttg.program import program
 
+hero = r"""
+ ______   ______   ______   
+/\__  _\ /\__  _\ /\  ___\  
+\/_/\ \/ \/_/\ \/ \ \ \__ \ 
+   \ \_\    \ \_\  \ \_____\
+    \/_/     \/_/   \/_____/
+
+"Truth Table Generator" by [bold]Theone Eclarin[/bold]
+   [bright_black]... Press (Cmd/Ctrl + C) to force-exit ...[/bright_black] """  # noqa: W291
+
 
 @click.command("tgg")
-@click.argument("input", required=True)
+@click.argument("input", required=False)
 @click.option("-f", "--file", is_flag=True, help="Treats the input as a filepath.")
 @click.option("-i", "--inspect", is_flag=True, help="Display debug data.")
 def command(input: str, file: bool = False, inspect: bool = False) -> None:
-    # Test if "formula" is actually a filepath
+    # If input is a filepath, read formulas from file
     if file:
         filepath = Path(input)
 
@@ -28,8 +38,25 @@ def command(input: str, file: bool = False, inspect: bool = False) -> None:
             sys.exit(-1)
 
         formulas = filepath.read_text().splitlines()
-    else:
-        formulas = [input]
+        for formula in formulas:
+            program(formula, inspect)
 
-    for formula in formulas:
-        program(formula, inspect)
+    # If input exists, assume its a formula and run program once
+    elif input:
+        program(input, inspect)
+
+    # Else, run program in interactive mode
+    else:
+        rich_console.print(hero)
+
+        while True:
+            rich_console.print()
+            formula = rich_console.input(
+                "Enter a [bright_magenta italic]formula[/bright_magenta italic]: ",
+            )
+            program(formula, inspect)
+
+            rich_console.print()
+            yesno = rich_console.input("Would you like to try again? (Y/N): ")
+            if yesno.lower() != "y":
+                break
